4569e38d137bfadf47ae5e07b03b9fd50c3f5370
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-19 10:02:34 +1100

    fix: image display

--- src/components/headerImages.jsx
+++ src/components/headerImages.jsx
@@ -4,11 +4,13 @@ import indoorpool from './images/indoorpool.png'
 export default function HeaderCarousel() {
   return (
     <Carousel data-bs-theme="dark">
-      <Carousel.Item >
+      <Carousel.Item  >
         <img
           className="d-block w-100"
-          src= {'./images/indoorpool.png'}
+          src= {require('./images/indoorpool.png')}
           alt="pool "
+          width={1710}
+          height={356}
 
         />
         <Carousel.Caption>
@@ -19,8 +21,10 @@ export default function HeaderCarousel() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=eee"
+          src= {require('./images/outdoorPool.png')}
           alt="Second slide"
+          width={1710}
+          height={356}
         />
         <Carousel.Caption>
           <h5>Second slide label</h5>
@@ -30,8 +34,10 @@ export default function HeaderCarousel() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
+          src={require('./images/pool.png')}
           alt="Third slide"
+          width={1710}
+          height={356}
         />
         <Carousel.Caption>
           <h5>Third slide label</h5>
