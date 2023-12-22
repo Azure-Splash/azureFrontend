06da08e0abf0686b8051fa0976965488888c7b31
Fabian <128451994+fabs-pe@users.noreply.github.com>
2023-12-22 23:03:10 +1000

    fix: unused vars

--- src/pages/LoginSignupPage.jsx
+++ src/pages/LoginSignupPage.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 import axios from 'axios'
  import Footer from "../components/Footer";
 import {login} from "../functions/login";
