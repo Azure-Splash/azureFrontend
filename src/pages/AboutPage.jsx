56b49d2ecffd12d8462c37a66a711b2b73c0be4c
claire-chua <126572960+claire-chua@users.noreply.github.com>
2024-01-04 17:38:33 +1100

    updated about summary

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,6 +8,9 @@ export default function AboutPage(props){
                         About
                         
                     </h1>
+                    <h5>
+                        Whether you're in search of a leisurely dip to unwind or seeking a more invigorating aquatic workout, our pools cater to all levels of experience and preferences.
+                    </h5>
                     <Footer/>
             </div>
            
