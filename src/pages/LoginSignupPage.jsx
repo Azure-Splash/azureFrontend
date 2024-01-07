5aac161545fa6228da985d6d64f7b584de73217f
Fabian <128451994+fabs-pe@users.noreply.github.com>
2024-01-07 22:37:57 +1000

    fix: netlify

--- src/pages/LoginSignupPage.jsx
+++ src/pages/LoginSignupPage.jsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 // import {Link} from "react-router-dom";
-import axios from 'axios'
+// import axios from 'axios'
  import Footer from "../components/Footer";
 import {login} from "../functions/login";
 import {Link} from "react-router-dom";
