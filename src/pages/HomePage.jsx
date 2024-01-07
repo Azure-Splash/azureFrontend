27d284b0a3351c67f00ef4ced932d8607050e4ce
claire-chua <clair3.chua@gmail.com>
2024-01-07 13:59:48 +1100

    updated home css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,22 +5,33 @@ export default function HomePage(props){
 	return(
 		<div>
 			<h1>
-
+				<br></br>
                 Azure Splash
                 
             </h1>
+			<br></br>
+			<div class="homedescription">
 			<p>
 				Get ready to immerse yourself into a world of  Whether you're in search of a leisurely dip to unwind or seeking a more invigorating aquatic workout, our pools cater to all levels of experience and preferences.
 			</p>
 			<p>
 				Simply create an account with us, log in and reserve a lane slot to begin your experience.
 			</p>
-			<div>
-				<h3>
+			</div>
+			<br></br>
+			<br></br>
+			<h3 class="comingsoontext">
+
 					Coming soon:
-				</h3>
-				<img src={require('../components/images/sauna.jpg')} alt="sauna"/>
-				<p> We are opening up a sauna in March 2024, adjacent to the indoor swimming pool. This is the perfect opportunity to release any tension, and improve cardiovascular health.</p>
+			</h3>
+			<br></br>
+			<div class = "aboutsection2">
+		
+				<div class="col-md-6">
+				<img src={require('../components/images/sauna.jpg')} class="saunaimg img-fluid " alt="sauna"/>
+				</div>
+				
+				<p class ="comingsoondescription"> We are opening up a sauna in March 2024, adjacent to the indoor swimming pool. This is the perfect opportunity to release any tension, and improve cardiovascular health.</p>
 			</div>
 			<main>
 				
