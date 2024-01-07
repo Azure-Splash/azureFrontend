c93a6fb553483b6effa34571d575751295662efa
cameronjohn89 <126525284+cameronjohn89@users.noreply.github.com>
2024-01-07 23:48:59 +1100

    Updated and fix media queries.

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -10,12 +10,12 @@ export default function ContactForm(props){
     const[message, setMessage] = useState('')
     
     return(
-        <div id ="contact-page">
+        <div id="contact-page">
 			<h2 id="contact-title">
 				Contact Us
 			</h2>
             
-            <div id ="contact-details" >
+            <div id="contact-details" >
             <div>
                 <h4>Address:</h4>
                 <h5> Come and visit us!</h5>
@@ -52,74 +52,74 @@ export default function ContactForm(props){
             </div>
             </div>
 
-        </div>
-        <div class="divider"></div>
-        <div class="contact-form-section">
-        <img src="mail-icon.png" alt="mail icon" class="mail-icon"/>
-        <div class="contact-form-subtitle">
-            
-            <h2>
+            </div>
+            <div class="divider"></div>
+            <div class="contact-form-section">
+            <img src="mail-icon.png" alt="mail icon" class="mail-icon"/>
+            <div class="contact-form-subtitle">
                 
-                "We'd love to hear from you! 
-                <div class="d-block mb-3"></div>
-                <p class="contact-form-subtitle-2">Please feel free to leave us a message."</p>
-            
-            </h2>
-        </div>
+                <h2>
+                    
+                    "We'd love to hear from you! 
+                    <div class="d-block mb-3"></div>
+                    <p class="contact-form-subtitle-2">Please feel free to leave us a message."</p>
+                
+                </h2>
+            </div>
         
         
         
-        <form id="contact-form" class="custom-container rounded custom-border mx-auto">
-        <div class="form-group">
-            <label>
-                <p class="text-left">Name:</p>
-                <input class="form-control form-control-lg"
-                type="name"
-                value={name}
-                onChange={(event)=>{setName(event.target.value)}} placeholder ="John Doe" />
-            </label>
-        </div>
-        <div class="form-group">
-            <label>
-                <p class="text-left">Email:</p>
-                <input  class="form-control form-control-lg"
-                type="text" 
-                value={email}
-                onChange={(event)=>{setEmail(event.target.value)}} placeholder ="*****@gmail.com" />
-            </label>
-        </div>
-        <div class="form-group">
-            <label>
-                <p class="text-left">Phone Number: </p>
-                <input class="form-control form-control-lg"
-                type="text"
-                value={phoneNumber}
-                onChange={(event)=>{setPhoneNumber(event.target.value)}} placeholder ="000" />
-            </label>
-        </div>
-        <div class="form-group">
-            <label>
-                <p class="text-left">Message: </p>
-                <textarea
-                    className="form-control-lg form-control"
-                    rows="5"
-                    id="comment"
-                    placeholder="Hi, please type in your query here."
-                    value={message}
-                    onChange={(event) => { setMessage(event.target.value) }}>
+            <form id="contact-form" class="custom-container rounded custom-border mx-auto">
+            <div class="form-group">
+                <label>
+                    <p class="text-left">Name:</p>
+                    <input class="form-control form-control-lg"
+                    type="name"
+                    value={name}
+                    onChange={(event)=>{setName(event.target.value)}} placeholder ="John Doe" />
+                </label>
+            </div>
+            <div class="form-group">
+                <label>
+                    <p class="text-left">Email:</p>
+                    <input  class="form-control form-control-lg"
+                    type="text" 
+                    value={email}
+                    onChange={(event)=>{setEmail(event.target.value)}} placeholder ="*****@gmail.com" />
+                </label>
+            </div>
+            <div class="form-group">
+                <label>
+                    <p class="text-left">Phone Number: </p>
+                    <input class="form-control form-control-lg"
+                    type="text"
+                    value={phoneNumber}
+                    onChange={(event)=>{setPhoneNumber(event.target.value)}} placeholder ="000" />
+                </label>
+            </div>
+            <div class="form-group">
+                <label>
+                    <p class="text-left">Message: </p>
+                    <textarea
+                        className="form-control-lg form-control"
+                        rows="5"
+                        id="comment"
+                        placeholder="Hi, please type in your query here."
+                        value={message}
+                        onChange={(event) => { setMessage(event.target.value) }}>
 
-                </textarea>
-            </label>
-        </div>
-       
-        <div class="form-group">
-            <button class="btn btn-info" onClick={() => {contact(email, name, phoneNumber, message)}}>
-                Submit
-            </button>
-        </div>
-        </form>
-        </div>
-        <Footer />
+                    </textarea>
+                </label>
+            </div>
+        
+            <div class="form-group">
+                <button class="btn btn-info" onClick={() => {contact(email, name, phoneNumber, message)}}>
+                    Submit
+                </button>
+            </div>
+            </form>
+            </div>
+            <Footer />
         </div>
         )
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,35 +5,37 @@ export default function HomePage(props){
 	return(
 		<div>
 			<div className="main-container">
-			<h1>
+				<h1>
+					<br></br>
+					Azure Splash
+					
+				</h1>
 				<br></br>
-                Azure Splash
-                
-            </h1>
-			<br></br>
-			<div class="homedescription">
-			<p>
-				Get ready to immerse yourself into a world of  Whether you're in search of a leisurely dip to unwind or seeking a more invigorating aquatic workout, our pools cater to all levels of experience and preferences.
-			</p>
-			<p>
-				Simply create an account with us, log in and reserve a lane slot to begin your experience.
-			</p>
-			</div>
-			<br></br>
-			<br></br>
-			<h3 class="comingsoontext">
-
-					Coming soon:
-			</h3>
-			<br></br>
-			<div class = "aboutsection2">
-		
-				<div class="col-md-6">
-				<img src={require('../components/images/sauna.jpg')} class="saunaimg img-fluid " alt="sauna"/>
+				<div class="homedescription">
+				<p>
+					Get ready to immerse yourself into a world of  Whether you're in search of a leisurely dip to unwind or seeking a more invigorating aquatic workout, our pools cater to all levels of experience and preferences.
+				</p>
+				<p>
+					Simply create an account with us, log in and reserve a lane slot to begin your experience.
+				</p>
 				</div>
-				
-				<p class ="comingsoondescription"> We are opening up a sauna in March 2024, adjacent to the indoor swimming pool. This is the perfect opportunity to release any tension, and improve cardiovascular health.</p>
-			</div>
+				<br></br>
+				<br></br>
+				<h3 class="comingsoontext">
+
+						Coming soon:
+				</h3>
+				<br></br>
+				<div class = "aboutsection2">
+			
+					<div class="col-md-6">
+					<img src={require('../components/images/sauna.jpg')} class="saunaimg img-fluid " alt="sauna"/>
+					</div>
+					
+					<div>
+					<p class ="comingsoondescription"> We are opening up a sauna in March 2024, adjacent to the indoor swimming pool. This is the perfect opportunity to release any tension, and improve cardiovascular health.</p>
+					</div>
+				</div>
 			</div>
 			<Footer/>
 		</div>
